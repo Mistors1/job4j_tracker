cca936a626aee439b0bef62033bc5b5fcf633aab
Mistors1 <Kolov-dm@bk.ru>
2023-01-23 18:51:25 +0300

    Error

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error lite = new Error();
+        Error hard = new Error(true, 115, "bad");
+        hard.printInfo();
+        lite.printInfo();
+    }
+
+}
