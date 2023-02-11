398bb598c3d1f433bcfe27e6f7b53ae1f79c5715
Mistors1 <Kolov-dm@bk.ru>
2023-02-11 17:16:53 +0300

    StartUI для программы Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,16 +1,98 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.util.Scanner;
 
 public class StartUI {
-    public static void main(String[] args) {
-        Item item = new Item();
-        LocalDateTime currentDateTime = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = currentDateTime.format(formatter);
-        System.out.println(currentDateTimeFormat);
-        System.out.println(item);
 
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new application ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Added application " + item);
+            } else if (select == 1) {
+                System.out.println("=== Show all applications ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("No applications.");
+                }
+            } else if (select == 2) {
+                System.out.println("=== Edit application ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.println("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.replace(id, item);
+            if (tracker.replace(id, item)) {
+                System.out.println("Application changed successfully.");
+            } else {
+                System.out.println("Application not found. ");
+            }
+            } else if (select == 3) {
+                System.out.println("=== Delete application ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("Application deleted successfully.");
+                } else {
+                    System.out.println("Application not found.");
+                }
+            } else if (select == 4) {
+                System.out.println("=== Find application by id ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Application with id " + id + " not found.");
+                }
+            } else if (select == 5) {
+                System.out.println("=== Find applications by name ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item[] item  = tracker.findByName(name);
+                if (item.length > 0) {
+                    for (Item names : item) {
+                        System.out.println(names);
+                    }
+                    } else {
+                    System.out.println("Applications with name: " + name + " not found.");
+                }
+            } else if (select == 6) {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new application", "Show all applications", "Edit application",
+                "Delete application", "Find application by id", "Find applications by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
