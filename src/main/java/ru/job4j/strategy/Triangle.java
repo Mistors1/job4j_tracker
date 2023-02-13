6a3056f8e9ebaea719d93efe77e0aee3e06ed6c9
Mistors1 <Kolov-dm@bk.ru>
2023-02-13 17:04:57 +0300

    Strategy

---
+++ src/main/java/ru/job4j/strategy/Triangle.java
@@ -0,0 +1,11 @@
+package ru.job4j.strategy;
+
+public class Triangle  implements Shape {
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    "    ^    " + ln
+                + "  /   \\ " + ln
+                + " /     \\" + ln
+                + " ------- ";
+    }
+}
