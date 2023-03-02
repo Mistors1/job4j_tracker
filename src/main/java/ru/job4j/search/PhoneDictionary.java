b6053d12f37f9eac7416baace511db802c0affc3
Mistors1 <Kolov-dm@bk.ru>
2023-03-02 16:52:44 +0300

    Lambda tasks

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,22 +1,26 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.*;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<>();
+    private final ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> predName = k -> k.getName().contains(key);
+        Predicate<Person> predPhone = k -> k.getPhone().contains(key);
+        Predicate<Person> predSurname = k -> k.getSurname().contains(key);
+        Predicate<Person> predAddress = k -> k.getAddress().contains(key);
+
+        Predicate<Person> combine = k -> predName.or(predPhone).or(predSurname).or(predAddress).test(k);
         ArrayList<Person> result = new ArrayList<>();
-        for (Person found : persons) {
-            if (found.getName().equals(key)
-                    || found.getSurname().equals(key)
-                    || found.getPhone().equals(key)
-                    || found.getAddress().equals(key)) {
-                result.add(found);
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
             }
         }
         return result;
