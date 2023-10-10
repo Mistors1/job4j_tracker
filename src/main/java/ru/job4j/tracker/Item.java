5634d42c1b8cae751fe8529d4e7f7d9c29c02237
Mistors1 <Kolov-dm@bk.ru>
2023-10-10 18:47:54 +0400

    HibernateTryUsage

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,78 +1,25 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Objects;
+import lombok.*;
 
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "items")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Item {
 
-    private LocalDateTime created = LocalDateTime.now();
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NonNull
     private String name;
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public Item(int id, String name, LocalDateTime created) {
-        this.name = name;
-        this.id = id;
-        this.created = created;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "created = " + created.format(FORMATTER)
-                + ", id = " + id
-                + ", name = '" + name
-                + '\'' + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(created, item.created) && Objects.equals(name, item.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(created, id, name);
-    }
+    @NonNull
+    private LocalDateTime created = LocalDateTime.now();
 }
 
